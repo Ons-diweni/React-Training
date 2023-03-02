b8ba30ece4fa76e67ae815a3476922662283c523
Ons <ons.diweni@esprit.tn>
2023-03-02 05:49:28 +0100

    updating

--- src/index.js
+++ src/index.js
@@ -14,7 +14,19 @@ root.render(
 );
 root.render(<Banner/> )
 root.render(<Cart/>)
+
+
+
+
+
+
+
+
+
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
