1b6486751a608d73fb6f85730ef9a253097c81a7
Ons <ons.diweni@esprit.tn>
2023-03-02 05:56:13 +0100

    updating

--- src/index.js
+++ src/index.js
@@ -9,11 +9,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Banner/> <Cart/>
   </React.StrictMode>
 );
-root.render(<Banner/> )
-root.render(<Cart/>)
+
 
 
 
