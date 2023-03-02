6a75293e3cea41e5721aa4f31f0c9a5c95acbe62
Ons <ons.diweni@esprit.tn>
2023-03-02 06:22:06 +0100

    adding style to the Banner Component

--- src/components/BannerComponent/Banner.js
+++ src/components/BannerComponent/Banner.js
@@ -1,5 +1,8 @@
+import './Banner.css'
+
+
 function Banner () {
-    return <h1> Hello From Banner </h1>
+    return <h1 className="style-banner"> Hello From Banner </h1>
 }
 
 export default Banner
