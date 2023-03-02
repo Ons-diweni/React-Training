6f64750b556da2e917b597826bf94d2c4a0bb23e
Ons <ons.diweni@esprit.tn>
2023-03-02 05:38:34 +0100

    clean structure

---
+++ src/components/CartComponent/Cart.js
@@ -0,0 +1,17 @@
+function cart () {
+
+const pinkPrice  = 12  ;
+const redPrice = 25 ; 
+const yellowPrice = 36 ; 
+
+return <ul> 
+<li> the pink price is {pinkPrice}</li>
+<li> the red price is {redPrice}</li>
+<li> the yellow price is {yellowPrice}</li>
+</ul>
+
+}
+
+
+
+export default cart
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/AppComponent/App';
 import Banner from './components/BannerComponent/Banner'
+import Cart from './components/CartComponent/Cart';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +13,7 @@ root.render(
   </React.StrictMode>
 );
 root.render(<Banner/> )
+root.render(<Cart/>)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
