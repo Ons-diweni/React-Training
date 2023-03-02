3f2f63aad850a322b82fbe794df91db1d6048234
Ons <ons.diweni@esprit.tn>
2023-03-02 07:03:00 +0100

    adding style

--- src/components/BannerComponent/Banner.js
+++ src/components/BannerComponent/Banner.js
@@ -1,8 +1,12 @@
 import './Banner.css'
+import logo from '../../assets/logo.png'
 
 
 function Banner () {
-    return <h1 className="style-banner"> Hello From Banner </h1>
+    const title = "Bienvenue chez le fleuriste des amoureux"
+    return  <div className='style-banner'> 
+        <img src={logo} alt='le logo du site' className="style-logo" />
+    <h1 className="style-title"> {title} </h1> </div>
 }
 
 export default Banner
