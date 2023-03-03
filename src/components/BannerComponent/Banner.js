83eaf3d883fd74870d5d8c7c5292d997757257c6
Ons <ons.diweni@esprit.tn>
2023-03-03 01:17:23 +0100

    updating

--- src/components/BannerComponent/Banner.js
+++ src/components/BannerComponent/Banner.js
@@ -8,9 +8,9 @@ function Banner () {
     <div className="style-banner">
       <div>
         <img src={logo} alt="le logo du site" className="style-logo" />
-        <h1 className="style-title"> {title} </h1>{' '}
+        <h1 className="style-title"> {title} </h1>
+        <div > <Recommendation /> </div>
       </div>
-      <div> <Recommendation /> </div>{' '}
     </div>
   );
 }
