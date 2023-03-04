880ca1bcf0c147fb5c4547ecadfca343f74d60dd
Ons <ons.diweni@esprit.tn>
2023-03-05 00:40:10 +0100

    useState

--- src/components/FormComponent/Form.js
+++ src/components/FormComponent/Form.js
@@ -2,20 +2,24 @@ import { useState } from "react";
 
 export const FormQuestion = () => {
   
-    function handleSubmit (e) {
+  /*   function handleSubmit (e) {
     e.preventDefault ();
     alert (e.target['Myinput'].value);
-  }
+  } */
+
+  //Le hook useState renvoie un tableau contenant deux éléments :
+  // la valeur actuelle de l'état et une fonction permettant de mettre à jour cette valeur
   const [inputValue, setInputValue] = useState('Posez votre question ici')
   return (
-   /*  <form  onSubmit={handleSubmit}>
+   
+    /*  <form  onSubmit={handleSubmit}>
       <input type={Text} name="Myinput" defaultValue="Tapez votre texte"/>
       <button type="text"> Submit </button>
     </form> */
 
 
     <div>
-    <textarea
+    <input
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
     />
