32fd4287227604202d2727b7f2a1f82440dd9b50
Ons <ons.diweni@esprit.tn>
2023-03-05 03:01:28 +0100

    useState

--- src/components/CareScaleComponent/careScale.js
+++ src/components/CareScaleComponent/careScale.js
@@ -1,7 +1,5 @@
 export const CareScale = ({scaleValue, careType}) => {
   
-
-
 const quantityLabel = {
         1: 'peu',
         2: 'modérément',
@@ -10,8 +8,8 @@ const quantityLabel = {
 const range = [1, 2, 3];
 const guessType = careType === 'light' ? '☀️' : '💧';
 
-return ( <div onClick={() => alert( `Cette plante requiert ${quantityLabel[scaleValue]} 
-${ careType === 'light' ? 'de lumière' : "d'arrosage" }`)}>
+return ( <div  onClick={() => alert( `Cette plante requiert ${quantityLabel[scaleValue]} 
+${ careType === 'light' ? 'de lumière' : "d'arrosage" }`)}> 
 {range.map(r => scaleValue >= r ? <span> {guessType} </span>  : null)}  </div> )
 
 };
