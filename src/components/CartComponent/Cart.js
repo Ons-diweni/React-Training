d65b4b35e73dec0e9baad3847939eb9923c4859e
Ons <ons.diweni@esprit.tn>
2023-03-05 22:46:17 +0100

    Cart

--- src/components/CartComponent/Cart.js
+++ src/components/CartComponent/Cart.js
@@ -10,14 +10,14 @@ const [isOpen, setIsOpen] = useState(false);
 return isOpen? 
 
 (<div>
-<button onClick={() => setIsOpen(false)} > Fermer </button>
+<button  className="button-closed" onClick={() => setIsOpen(false)} > Fermer </button>
 <div className="Cart">
  <h1> Panier : </h1>
  Monstera : {monsteraPrice}€
- <button  onClick={() => updateCart(cart+1)} >  add to cart </button>
+ <button className="add-button"  onClick={() => updateCart(cart+1)} >  add to cart </button>
  <h3> Totale : {cart*monsteraPrice} </h3>
 
-</div> </div>) : (<div> <button onClick={() => setIsOpen(true)}  > Ouvrir </button> </div>)
+</div> </div>) : (<div> <button className="toggle-button" onClick={() => setIsOpen(true)}  > Ouvrir </button> </div>)
 
 }
 
