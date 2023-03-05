6920a0f9d2d63caa0da371955cf2c078c5ab2002
Ons <ons.diweni@esprit.tn>
2023-03-05 22:26:41 +0100

    Cart

--- src/components/CartComponent/Cart.js
+++ src/components/CartComponent/Cart.js
@@ -5,14 +5,19 @@ export const  Cart = () => {
 
 const monsteraPrice = 8
 const [cart, updateCart] = useState(0);
+const [isOpen, setIsOpen] = useState(false);
 
-return (<div className="Cart">
+return isOpen? 
+
+(<div>
+<button onClick={() => setIsOpen(false)} > Fermer </button>
+<div className="Cart">
  <h1> Panier : </h1>
  Monstera : {monsteraPrice}€
  <button  onClick={() => updateCart(cart+1)} >  add to cart </button>
  <h3> Totale : {cart*monsteraPrice} </h3>
 
-</div>)
+</div> </div>) : (<div> <button onClick={() => setIsOpen(true)}  > Ouvrir </button> </div>)
 
 }
 
